97e6eb9bae60fe8a958255a2a905295188b26a1c
prostoandrei <prostoandrei91@gmail.com>
2020-03-09 10:36:09 +0300

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,21 @@
-function sum(a, b) {
-  /* ваш код */
-}
+/**
+ * Возвращает сумму двух чисел
+ *
+ * @param {number} a
+ * @param {number} b
+ * @throws Выбрасывает ошибку, если один из аргументов не является числом
+ * @return {number}
+ */
+const sum = (a, b) => {
+  if (!Number.isInteger(a)) {
+    throw new TypeError('TypeError: a is not a number. Argument "a" is: ' + typeof(a));
+  }
+
+  if (!Number.isInteger(b)) {
+    throw new TypeError('TypeError: b is not a number. Argument "b" is: ' + typeof(b));
+  }
+
+  return a + b;
+};
 
 module.exports = sum;
