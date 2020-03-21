5b779e2b5165bc3fe11fab8ebba34e96de96c70e
prostoandrei <prostoandrei91@gmail.com>
2020-03-21 10:37:29 +0300

    added 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,18 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const isNestedPath = pathname.split('/').length >= 2;
+
+  if (isNestedPath) {
+    res.statusCode = 400;
+    res.end('Nested path is deprecated');
+  }
 
   switch (req.method) {
-    case 'GET':
+    case 'GET': {
+      fs.access(filepath, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('File doesn\'t exist');
+        } else {
+          fs.createReadStream(filepath).pipe(res);
+        }
+      });
 
       break;
+    }
 
     default:
       res.statusCode = 501;
