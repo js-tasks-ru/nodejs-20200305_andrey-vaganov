dc71d6add50795e298df1c8e5625a2a3ef0d4846
prostoandrei <prostoandrei91@gmail.com>
2020-03-29 17:02:29 +0300

    added solution to 5 module

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,25 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let clients = [];
+
+const addClient = async () => new Promise((resolve) =>
+  clients.push(resolve)
+);
+
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await addClient();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (message) {
+    ctx.status = 200;
+    await clients.map((resolve) => resolve(message));
+
+    clients = [];
+  }
 });
 
 app.use(router.routes());
